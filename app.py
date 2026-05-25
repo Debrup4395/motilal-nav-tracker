8c726b2642d3c20ab92478ddaff5bcbd758f2981
SOUMOSHREE MALLIK <kingdebrup@gmail.com>
2026-05-25 09:12:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -145,8 +145,8 @@ with col_title:
 # MANUAL NAV UPDATE
 # =========================
 
-previous_nav = 106.25
-weekly_start_nav = 104.72
+previous_nav = 105.99
+weekly_start_nav = 105.99
 
 # =========================
 # INVESTMENT DETAILS
